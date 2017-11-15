de4161b6edf33e5e69fa7c6385cafb34a2696c5e
Connie Lee <connie.lee@fullstackacademy.com>
2017-11-15 00:11:33 -0600

    Set up utils

---
+++ src/utils/index.js
@@ -0,0 +1,13 @@
+export const generateRoomCode = () => {
+  // 4-long alphanumeric string
+  // must not conflict with a currectly active code
+};
+
+export const generateBoard = () => {
+  // randomly select 25 unique words from words list to form 5x5 array
+};
+
+export const generateKeyCard = () => {
+  // create 5x5 grid with slots labeled for RED, BLUE, NEUTRAL, or ASSASSIN
+  // determine which team goes first and assign spots accordingly
+};
