8f6178732635f23d854d523d6be9c546af129fb2
Connie Lee <connie.lee@fullstackacademy.com>
2017-12-07 12:13:09 -0600

    Style CurrentTurn

--- src/components/gameView/CurrentTurn.js
+++ src/components/gameView/CurrentTurn.js
@@ -8,6 +8,33 @@ import Button from 'material-ui/Button';
 
 import { endTurn } from '../../reducers/actionCreators';
 
+const styles = {
+  title: {
+    fontFamily: 'Space Mono',
+    padding: '1vh 0',
+    fontWeight: 'bold',
+  },
+  contentContainer: {
+    padding: '1vh 5vw',
+  },
+  largerFont: {
+    fontSize: '1rem',
+  },
+  padBottom: {
+    paddingBottom: '3vh',
+  },
+  redText: {
+    color: '#F44336',
+  },
+  blueText: {
+    color: '#3F51B5',
+  },
+  teamText: {
+    fontFamily: 'Space Mono',
+    fontWeight: 'bold',
+  },
+};
+
 const mapState = state => ({
   team: state.currentTurn.team,
   clue: state.currentTurn.clue,
@@ -20,20 +47,27 @@ const mapDispatch = dispatch => ({
 
 const CurrentTurn = ({ team, clue, number, turnIsOver, end }) => (
   <Card className="panel-card">
-    <CardContent>
-      <Typography type="headline" component="h2">CURRENT TURN</Typography>
-      <div>
-        <Typography component="p">TEAM: {team}</Typography>
-        {(clue && number) ?
-          <div>
-            <Typography component="p">
-              CLUE: <span className="code">{clue}</span> for <span className="code">{number}</span>
-            </Typography>
-          </div> :
-          <Typography component="p">WAITING ON SPYMASTER</Typography>}
+    <CardContent className="text-center">
+      <Typography type="headline" component="h2" style={styles.title}>CURRENT TURN</Typography>
+      <div style={styles.contentContainer}>
+        <Typography component="p" style={styles.largerFont}>TEAM:
+          <span style={styles.teamText}> {team} </span>
+        </Typography>
+        <Typography component="p" style={Object.assign({}, styles.largerFont, styles.padBottom)}>
+          {(clue && number) ?
+            <span>
+              CLUE:
+              <span className="code"> {clue} </span>
+              for
+              <span className="code"> {number} </span>
+            </span> :
+            'WAITING ON SPYMASTER'}
+        </Typography>
         {turnIsOver ?
           <div>
-            <Typography type="headline">YOUR TURN IS OVER</Typography>
+            <Typography component="p" style={styles.largerFont}>
+              YOUR TURN IS OVER
+            </Typography>
             <Button raised onClick={end}>OK</Button>
           </div> :
           <Button raised onClick={end}>PASS</Button>}
